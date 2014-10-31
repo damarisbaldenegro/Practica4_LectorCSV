f5087ec22ce6e1eb85165a3feb5d935284f7b2b5
damaris Baldenegro <damarisbaldenegro@gmail.com>
2014-10-30 18:36:22 -0600

    Primera version

---
+++ LectordeCSV/Program.cs
@@ -0,0 +1,35 @@
+﻿
+using System;
+using System.IO;
+using System.Collections;
+
+namespace LectordeCSV
+{
+	class Program{
+/// <summary>
+	/// Summary description for Class1.
+	/// </summary>
+	class Class1
+	{
+		static void Main(string[] args)
+		{
+			
+			StreamReader objReader = new StreamReader("c:\\ejemplo.txt");
+			string sLine="";
+			ArrayList arrText = new ArrayList();
+
+			while (sLine != null)
+			{
+				sLine = objReader.ReadLine();
+				if (sLine != null)
+					arrText.Add(sLine);
+			}
+                        objReader.Close();
+
+			foreach (string sOutput in arrText)
+				Console.WriteLine(sOutput);
+			Console.ReadLine();
+		}
+	}
+}
+}
